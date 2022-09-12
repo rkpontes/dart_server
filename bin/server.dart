610f266cecc30f921675d2dfb3843021dd740dd1
Raphael Pontes <raphaelkennedy@hotmail.com>
2022-09-12 17:49:55 -0300

    dependency injector inserted

--- bin/server.dart
+++ bin/server.dart
@@ -2,38 +2,17 @@ import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
 
-import '../src/app/data/repositories/products_impl_repository.dart';
-import '../src/app/domain/usecases/create_product_impl_usecase.dart';
-import '../src/app/domain/usecases/get_all_products_impl_usecase.dart';
-import '../src/app/domain/usecases/get_product_by_id_impl_usecase.dart';
-import '../src/app/external/datasources/products_impl_datasource.dart';
-import '../src/app/presentation/home/home_controller.dart';
-import '../src/app/presentation/products/products_controller.dart';
-
-final router = Router();
+import '../src/app/app_initialize.dart';
 
 void main(List<String> args) async {
-  // TODO: inserir injeção de dependencias
-  final productsImplDatasource = ProductsImplDatasource();
-  final productsImplRepository = ProductsImplRepository(productsImplDatasource);
-  final productsController = ProductsController(
-    GetAllProductsImplUsecase(productsImplRepository),
-    GetProductByIdImplUsecase(productsImplRepository),
-    CreateProductImplUsecase(productsImplRepository),
-  );
-
-  router.get("/", HomeController());
-  router.get("/products", productsController.getAllProducts);
-  router.get("/products/<id>", productsController.getproductById);
-  router.post("/products", productsController.createProduct);
+  var app = AppInitialize();
 
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(app.get());
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
